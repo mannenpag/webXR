562878f238f2b53c613f10067e8c14b415ca99d5
Gustav <gustav.bodin1@gmail.com>
2020-03-16 17:51:29 +0100

    Components added

---
+++ midterm/components/ripple.js
@@ -0,0 +1,30 @@
+AFRAME.registerComponent('ripple', {
+    schema: {
+        amount: {type: 'number', default: 4},
+        radiusInner: {type: 'number', default: 1.28},
+        radiusOuter: {type: 'number', default: 1.3},
+        color: {type: 'color', default: '#FAFAFA'},
+    }, 
+
+    init: function () {
+        let data = this.data;
+        let el = this.el;
+
+        for (let i = 0; i < this.data.amount; i++) {
+            let circle = document.createElement('a-entity');
+            circle.setAttribute('position', '0 -1 0');
+            circle.setAttribute('rotation', '90 90 90');
+            circle.setAttribute('geometry', {
+                primitive: 'ring',
+                radiusInner: data.radiusInner - (i*0.2), 
+                radiusOuter: data.radiusOuter - (i*0.2)
+            })
+            circle.setAttribute('material', {
+                side: 'double',
+                color: data.color
+            })
+            el.appendChild(circle);
+
+        }
+    }
+})
---
+++ midterm/components/sphere.js
@@ -0,0 +1,56 @@
+AFRAME.registerComponent('sphere-properties', {
+    schema: {
+        radius: {type: 'number', default: 1},
+        widthSegments: {type: 'number', default: 1},
+        heightSegments: {type: 'number', default: 1},
+        color: {type: 'color', default: '#FAFAFA'},
+        x: {type: 'number', default: 0},
+        y: {type: 'number', default: 0},
+        z: {type: 'number', default: 0}
+    },
+
+    init: function () {
+        let data = this.data;
+        let el = this.el;
+        let active = false;
+
+        el.setAttribute('position', {
+            x: this.data.x,
+            y: this.data.y,
+            z: this.data.z
+        })
+        el.setAttribute('radius', data.radius);
+        el.setAttribute('segments-height', data.widthSegments);
+        el.setAttribute('segments-width', data.heightSegments);
+        el.setAttribute('color', data.color);
+
+        el.addEventListener('click', function () {
+            if (!active) {
+                el.setAttribute('segments-height', 32);
+                el.setAttribute('segments-width', 32);
+                el.setAttribute('color', '#f6da86');
+                console.log("Active");
+
+                el.setAttribute('animation', {
+                    property: 'radius',
+                    to: '1.1',
+                    loop: 'true',
+                    elasticity: '400',
+                    easing: 'easeInSine',
+                    dur: '400',
+                });
+            }
+            if (active) {
+                el.setAttribute('segments-height', data.widthSegments);
+                el.setAttribute('segments-width', data.heightSegments);
+                el.setAttribute('color', data.color);
+                console.log("Not Active");
+
+                el.removeAttribute('animation');
+            }
+            active = !active;
+        })
+    },
+});
+
+
